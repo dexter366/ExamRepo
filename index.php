1a8c748fb23d7c8035474f1246d7c657951bc8aa
dexter366 <deithergalleon31@gmail.com>
2026-09-01 19:38:18 -0700

    Initial commit: Added login form

---
+++ index.php
@@ -0,0 +1,24 @@
+~<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>LOGIN FORM</h1>
+
+
+
+
+<input type="text">  Username
+
+
+<input type="text"> Password
+
+<input type="button" > Submit
+
+
+</body>
+</html>
